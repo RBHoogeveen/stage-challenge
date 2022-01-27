790d94d3de09a1f8029f1ce443afbc43471c3ac6
RBHoogeveen <rbhoogeveen@gmail.com>
2022-01-27 11:04:38 +0100

    Styling + README.md update

--- resources/views/challenge.blade.php
+++ resources/views/challenge.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="panel-body">
-        <h1>{{ $totalscore }}</h1>
+    <div style="position: absolute; top: 30%; left: 42%;" class="panel-body">
+        {{-- Total score: 3364 --}}
+        <h1 style="font-size: 72px; font-family: sans-serif; color:rgb(242, 71, 37); margin: 0;">&#11088; {{ $totalscore }} &#11088;</h1>
     </div>
 @endsection
